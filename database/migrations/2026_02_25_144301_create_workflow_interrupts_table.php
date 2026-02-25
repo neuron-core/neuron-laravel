927407504bafe0327648ae68595f20ac9efbe534
Valerio Barbera <valerio@inspector.dev>
2026-02-25 18:30:20 +0100

    eloquent workflow persistence migration

---
+++ database/migrations/2026_02_25_144301_create_workflow_interrupts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workflow_interrupts', function (Blueprint $table) {
+            $table->id();
+            $table->string('workflow_id')->unique();
+            $table->longText('interrupt')->charset('binary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workflow_interrupts');
+    }
+};
